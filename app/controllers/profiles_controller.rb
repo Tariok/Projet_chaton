a9db91d31352d8d09ef87af5527ec402c938c846
Ophélie Gaudin <ophelie.gaudin@outlook.fr>
2021-12-03 11:27:02 +0100

    Look orders on Profile#show

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,9 @@ class ProfilesController < ApplicationController
     @user = current_user
     @user_email = current_user.email
 
-    @user_orders = Order.select{ |order| order.user_id = current_user.id }
+    @user_orders = Order.where(user_id: current_user.id)
+    puts "########################################"
+    puts @user_orders
     
 
 
